a2cbba1be29e7b7aca854c9440f407af42ec4280
Doriane QUARTINO <dquartin@e1r9p12.42.fr>
2018-01-05 16:50:53 +0100

    modification du end

--- 21sh/srcs/arrow_move.c
+++ 21sh/srcs/arrow_move.c
@@ -6,7 +6,7 @@
 /*   By: dquartin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/13 16:49:58 by dquartin          #+#    #+#             */
-/*   Updated: 2018/01/05 16:44:36 by dquartin         ###   ########.fr       */
+/*   Updated: 2018/01/05 16:49:39 by dquartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ int		go_to_right(int i, int x, int prompt, struct winsize size)
 	if (i < x)
 	{
 		if ((i + prompt + 1) % size.ws_col == 0)
-			tputs(tgetstr("do", NULL), 1, ft_putin);
+			tputs(tgetstr("do", NULL), 100, ft_putin);
 		else
-			tputs(tgetstr("nd", NULL), 1, ft_putin);
+			tputs(tgetstr("nd", NULL), 100, ft_putin);
 		i++;
 	}
 	return (i);
@@ -29,7 +29,7 @@ int		go_to_left(int i)
 {
 	if (i > 0)
 	{
-		tputs(tgetstr("le", NULL), 1, ft_putin);
+		tputs(tgetstr("le", NULL), 100, ft_putin);
 		i--;
 	}
 	return (i);
@@ -56,27 +56,29 @@ int		home(int i, int x, struct winsize size, char **environ)
 	return (i);
 }
 
-int		end(int i, int x, struct winsize size, char **environ)
+int        end(int i, int x, struct winsize size, char **environ)
 {
-	int		prompt;
-	int		line;
+	int        prompt;
 
-	line = 0;
 	i = home(i, x, size, environ);
 	prompt = ft_atoi(ft_getenv(environ, "PROMPT"));
 	if (x > size.ws_col)
 	{
-		line = x / size.ws_col + 1;
 		if (i >= x)
 			return (i);
 		while (x - i > size.ws_col)
 		{
-			tputs(tgetstr("do", NULL), 100, ft_putin);
+			tputs(tgetstr("do", NULL), 1, ft_putin);
 			i = size.ws_col + i;
+		}
+		if ((x + prompt) % size.ws_col < prompt)
+		{
+			tputs(tgetstr("do", NULL), 1, ft_putin);
+			i = i + size.ws_col;
 		}
 		while (i < x + prompt)
 		{
-			tputs(tgetstr("nd", NULL), 100, ft_putin);
+			tputs(tgetstr("nd", NULL), 1, ft_putin);
 			i++;
 		}
 		i = x;
@@ -85,7 +87,7 @@ int		end(int i, int x, struct winsize size, char **environ)
 	{
 		while (i < x)
 		{
-			tputs(tgetstr("nd", NULL), 100, ft_putin);
+			tputs(tgetstr("nd", NULL), 1, ft_putin);
 			i++;
 		}
 	}
